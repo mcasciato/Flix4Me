3a6254e5e183d042384824ddfe001706ebff833f
mcasciato <michael.casciato@icloud.com>
2016-11-02 16:53:05 -0400

    updated user/movie associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161102203030) do
+ActiveRecord::Schema.define(version: 20161102204553) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "movies", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "title"
+    t.integer  "year"
+    t.string   "genre"
+    t.text     "plot"
+    t.integer  "rating"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
