bbaa9c0b6b311242d657fd39ce9c9be4e8a62029
mcasciato <michael.casciato@icloud.com>
2016-11-02 17:17:07 -0400

    added resources for movies and tv shows

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :movies, only: [:create, :update, :destroy, :index, :show]
+  resources :tv_shows, only: [:create, :update, :destroy, :index, :show]
 end
