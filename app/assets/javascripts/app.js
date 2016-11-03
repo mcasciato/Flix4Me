c548f9ef85d03aac6d4e1cb8b57c28d1fdc522d9
mcasciato <michael.casciato@icloud.com>
2016-11-03 13:34:52 -0400

    created app.js - referenced app in application.html

---
+++ app/assets/javascripts/app.js
@@ -0,0 +1,10 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('flix4me', ['ui.router', 'templates', 'Devise'])
+        .config(['$httpProvider', function($httpProvider) {
+            // for CSRF errors
+            $httpProvider.defaults.headers.common['X-CSRF-Token'] = $('meta[name=csrf-token]').attr('content');
+        }]);
+}())
