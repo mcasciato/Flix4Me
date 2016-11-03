754b14826c0973751547bfd790179c688e3a70f1
mcasciato <michael.casciato@icloud.com>
2016-11-03 13:42:38 -0400

    signup route coded

--- app/assets/javascripts/routes.js
+++ app/assets/javascripts/routes.js
@@ -10,5 +10,15 @@
                     templateUrl: 'home/home.html',
                     controller: 'HomeController as homeCtrl'
                 })
-        })
+                .state('home.signup', {
+                    url: 'signup',
+                    templateUrl: 'auth/signup.html',
+                    controller: 'AuthController as authCrl'
+                    onEnter: ['$state', 'Auth', function($state, Auth) {
+                        Auth.currentUser().then(function(){
+                            $state.go('home.mylist');
+                        });
+                    }]
+                })
+        })      .state()
 }())
