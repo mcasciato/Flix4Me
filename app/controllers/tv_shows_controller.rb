98cd8bd00260fdef8cf68db827c8bca109159b27
mcasciato <michael.casciato@icloud.com>
2016-11-02 21:21:06 -0400

    defined tv_show#create and strong params

--- app/controllers/tv_shows_controller.rb
+++ app/controllers/tv_shows_controller.rb
@@ -10,4 +10,29 @@ class TvShowsController < ApplicationController
         tv_show = TvShow.find_by_id(params[:id])
         render json: tv_show
     end
+
+    def create
+        tv_show = current_user.tv_shows.create(tv_show_params)
+        if tv_show.save
+            render json: { status: 201 }
+        else
+            render json:
+                { errors: tv_show.errors.full_messages },
+                status: 422
+        end
+    end
+
+    def update
+
+    end
+
+    def destroy
+
+    end
+
+    private
+
+    def tv_show_params
+        params.require(:tv_show).permit(:title, :year, :genre, :plot, :rating)
+    end
 end
