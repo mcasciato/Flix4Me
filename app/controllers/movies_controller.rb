3709ca2e30937ca40be2e3f4430a3e4032f2e127
mcasciato <michael.casciato@icloud.com>
2016-11-02 22:06:35 -0400

    defined movies#update

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -12,12 +12,23 @@ class MoviesController < ApplicationController
     end
 
     def create
-        movie = current_user.movies.create(movie_movie_params)
+        movie = current_user.movies.create(movie_params)
         if movie.save
             render json: { status: 201 }
         else
             render json:
                 { errors: movies.errors.full_messages },
+                status: 422
+        end
+    end
+
+    def update
+        movie = Movie.find_by_id(params[:id])
+        if movie.update(movie_params)
+            render json: { status: 201 }
+        else
+            render json:
+                { errors: movie.errors.full_messages },
                 status: 422
         end
     end
