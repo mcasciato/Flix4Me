6f688b67db32da16d9636f76795e47886e0eafe2
mcasciato <michael.casciato@icloud.com>
2016-11-02 22:04:15 -0400

    defined movies#create and strong params

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -10,4 +10,21 @@ class MoviesController < ApplicationController
         movie = Movie.find_by_id(params[:id])
         render json: movie
     end
+
+    def create
+        movie = current_user.movies.create(movie_movie_params)
+        if movie.save
+            render json: { status: 201 }
+        else
+            render json:
+                { errors: movies.errors.full_messages },
+                status: 422
+        end
+    end
+
+    private
+
+    def movie_params
+        params.require(:movie).permit(:title, :year, :genre, :plot, :rating)
+    end
 end
