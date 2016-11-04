5dc4e38c779b47c8d2b54aef5dd643cdbe256f47
mcasciato <michael.casciato@icloud.com>
2016-11-04 10:11:03 -0400

    added login route

--- app/assets/javascripts/routes.js
+++ app/assets/javascripts/routes.js
@@ -20,5 +20,16 @@
                         });
                     }]
                 })
-        })      .state()
+                .state('home.login', {
+                    url: 'login',
+                    templateUrl: 'auth/login.html',
+                    controller: 'AuthController as authCtrl',
+                    onEnter: ['$state', 'Auth', function($state, Auth) {
+                        Auth.currentUser().then(function(){
+                            $state.go('home.mylist');
+                        });
+                    }]
+                })
+                
+        })
 }())
