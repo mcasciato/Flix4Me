871c2d444e000a08360e0a9be0d9494bb663b4af
mcasciato <michael.casciato@icloud.com>
2016-11-04 11:45:57 -0400

    more work on HomeController

--- app/assets/javascripts/home/home.controller.js
+++ app/assets/javascripts/home/home.controller.js
@@ -1,7 +1,27 @@
 (function() {
 
     function HomeController() {
+        var vm = this;
 
+        vm.signedIn = Auth.isAuthenticated;
+        vm.logout = Auth.logout;
+        vm.getCurrentUser = getCurrentUser;
+
+        activate();
+
+        function activate() {
+            getCurrentUser;
+        };
+
+        function getCurrentUser() {
+            return Auth.currentUser()
+                       .then(setCurrentUser);
+        };
+
+        function setCurrentUser(user) {
+            console.log(user);
+            return vm.user = user;
+        };
 
     };
 
