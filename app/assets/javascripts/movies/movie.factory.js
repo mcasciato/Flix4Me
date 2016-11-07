aa7aa280a041904cf486d16a282b415a7fb5bdf9
mcasciato <michael.casciato@icloud.com>
2016-11-07 10:56:43 -0500

    wrapped movie factory in an IIFE

--- app/assets/javascripts/movies/movie.factory.js
+++ app/assets/javascripts/movies/movie.factory.js
@@ -1,27 +1,29 @@
-function MovieFactory($http) {
-    return {
-        getMovies: getMovies
-    }
+(function() {
+    function MovieFactory($http) {
+        return {
+            getMovies: getMovies
+        }
 
-    function getMovies() {
-        return $http.get('/movies')
-                    .then(handleSuccess)
-                    .catch(handleError);
+        function getMovies() {
+            return $http.get('/movies')
+                        .then(handleSuccess)
+                        .catch(handleError);
+        };
+
+        function handleSuccess(response) {
+            console.log(response);
+            return response.data;
+        };
+
+        function handleError(error) {
+            console.log(error);
+        };
+
     };
 
-    function handleSuccess(response) {
-        console.log(response);
-        return response.data;
-    };
+    MovieFactory.$inject = ['$http'];
 
-    function handleError(error) {
-        console.log(error);
-    };
-
-};
-
-MovieFactory.$inject = ['$http'];
-
-angular
-    .module('flix4me')
-    .factory('MovieFactory', MovieFactory);
+    angular
+        .module('flix4me')
+        .factory('MovieFactory', MovieFactory);
+}());
