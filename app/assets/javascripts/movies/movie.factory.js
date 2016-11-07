49ac5366b66a6e9fd1428c7efa06e67957f9a7ab
mcasciato <michael.casciato@icloud.com>
2016-11-07 10:46:03 -0500

    created movie factory

---
+++ app/assets/javascripts/movies/movie.factory.js
@@ -0,0 +1,27 @@
+function MovieFactory($http) {
+    return {
+        getMovies: getMovies
+    }
+
+    function getMovies() {
+        return $http.get('/movies')
+                    .then(handleSuccess)
+                    .catch(handleError);
+    };
+
+    function handleSuccess(response) {
+        console.log(response);
+        return response.data;
+    };
+
+    function handleError(error) {
+        console.log(error);
+    };
+
+};
+
+MovieFactory.$inject = ['$http'];
+
+angular
+    .module('flix4me')
+    .factory('MovieFactory', MovieFactory);
