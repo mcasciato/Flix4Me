321c1c0ab8734d5ae37b3aeb66c3b5b0c8155ff0
mcasciato <michael.casciato@icloud.com>
2016-11-07 11:51:53 -0500

    added more functions to movie factory

--- app/assets/javascripts/movies/movie.factory.js
+++ app/assets/javascripts/movies/movie.factory.js
@@ -1,14 +1,73 @@
 (function() {
+
+    'use strict';
+
     function MovieFactory($http) {
         return {
-            getMovies: getMovies
-        }
+                  getMovies: getMovies,
+                  getMovie: getMovie,
+                  createMovie: createMovie,
+                  updateMovie: updateMovie,
+                  destroyMovie: destroyMovie
+              }
 
-        function getMovies() {
-            return $http.get('/movies')
-                        .then(handleSuccess)
-                        .catch(handleError);
-        };
+              function getMovies() {
+                  return $http.get('/movies')
+                      .then(handleSuccess)
+                      .catch(handleError);
+              };
+
+              function getMovie(id) {
+                  return $http.get('/movies/' + id)
+                      .then(handleSuccess)
+                      .catch(handleError)
+              };
+
+              function createMovie(movie) {
+                  var req = {
+                      method: 'POST',
+                      url: '/movies',
+                      headers: {
+                          'Content-Type': 'application/json'
+                      },
+                      data: {
+                          movie: movie
+                          // the 'movie' data object {
+                          //    name: 'Evolution Craft Brewing',
+                          //    state_id: 20
+                          //}
+                      }
+                  };
+
+                  return $http(req)
+                             .then(handleSuccess)
+                             .catch(handleError)
+
+              };
+
+              function updateMovie(movie) {
+                  var req = {
+                      method: 'PATCH',
+                      url: '/movies/' + movie.id,
+                      headers: {
+                          'Content-Type': 'application/json'
+                      },
+                      data: {
+                        movie: movie
+                      }
+                  };
+
+                  return $http(req)
+                             .then(handleSuccess)
+                             .catch(handleError)
+              };
+
+              function destroyMovie(id) {
+                  return $http.delete('/movies/' + id)
+                      .then(handleSuccess)
+                      .catch(handleError)
+              };
+
 
         function handleSuccess(response) {
             console.log(response);
