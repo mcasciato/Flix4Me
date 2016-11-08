b4ed5b437ee7e7a6ef7e7d0736ba5b199968c845
mcasciato <michael.casciato@icloud.com>
2016-11-08 11:30:10 -0500

    defined pane in pane component

--- app/assets/javascripts/components/tab-component/pane.component.js
+++ app/assets/javascripts/components/tab-component/pane.component.js
@@ -2,6 +2,17 @@
 
     'use strict';
 
+    var pane = {
+        transclude: true,
+        require: {
+            TabsController: '^tabs'
+        },
+        bindings: {
+            title: '@'
+        },
+        controller: PaneController,
+        templateUrl: 'components/tab-component/pane.html'
+    }
     
     angular
         .module('flix4me')
