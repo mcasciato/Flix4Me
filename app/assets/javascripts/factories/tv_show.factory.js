5c079b3cdc905378c604a15f37c4a5122839ec59
mcasciato <michael.casciato@icloud.com>
2016-11-08 12:31:00 -0500

    finished tv show factory

--- app/assets/javascripts/factories/tv_show.factory.js
+++ app/assets/javascripts/factories/tv_show.factory.js
@@ -2,6 +2,26 @@
 
     'use strict';
 
+    function TvShowFactory($http) {
+        return {
+            getTvShows: getTvShows
+        }
+
+        function getTvShows() {
+            return $http.get('/tv_shows')
+                        .then(handleSuccess)
+                        .catch(handleError)
+        }
+
+        function handleSuccess(response) {
+            console.log(response)
+            return response.data
+        }
+
+        function handleError(error) {
+            console.log(error)
+        }
+    }
 
     angular
         .module('flix4me')
