9fd68ef8540e181facf8b8d3b7454a56b81c5a45
mcasciato <michael.casciato@icloud.com>
2016-11-08 11:59:14 -0500

    defined getMovies function in movie factory

--- app/assets/javascripts/factories/movie.factory.js
+++ app/assets/javascripts/factories/movie.factory.js
@@ -1,7 +1,17 @@
 (function(){
     'use strict';
 
+    function MovieFactory($http) {
+        return {
+            getMovies: getMovies
+        }
 
+        function getMovies() {
+            return $http.get('/movies')
+                        .then(handleSuccess)
+                        .catch(handleError)
+        }
+    }
 
     angular
         .module('flix4me')
