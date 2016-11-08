84a657412462ae7888e1e20370cb6e91351f88e9
mcasciato <michael.casciato@icloud.com>
2016-11-08 12:02:43 -0500

    stubbed out movies var and controller in movies.component.js

--- app/assets/javascripts/components/movies/movies.component.js
+++ app/assets/javascripts/components/movies/movies.component.js
@@ -2,7 +2,14 @@
 
     'use strict';
 
+    var movies = {
 
+    }
+
+    function MovieComponentController(MovieFactory) {
+
+    }
+    
     angular
         .module('flix4me')
         .component('movies', movies)
