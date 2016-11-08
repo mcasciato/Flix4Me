d368de7d1e23d2eb0731cfc10c87318f0b449157
mcasciato <michael.casciato@icloud.com>
2016-11-08 10:40:46 -0500

    updated navbar and home controller

--- app/assets/javascripts/home/home.controller.js
+++ app/assets/javascripts/home/home.controller.js
@@ -2,44 +2,16 @@
 
     'use strict';
 
-    function HomeController($scope, Auth, $state) {
-        var vm = this;
+    function HomeController($scope) {
+        $scope.name = 'Michael is Home'
+    }
 
-        vm.signedIn = Auth.isAuthenticated;
-        vm.logout = Auth.logout;
-        vm.getCurrentUser = getCurrentUser;
-
-        activate();
-
-        function activate() {
-            getCurrentUser;
-        };
-
-        function getCurrentUser() {
-            return Auth.currentUser()
-                       .then(setCurrentUser);
-        };
-
-        function setCurrentUser(user) {
-            console.log(user);
-            return vm.user = user;
-        };
-
-        $scope.$on('devise:new-registration', function(e, user) {
-            return vm.user = user;
-        });
-        $scope.$on('devise:login', function(e, user){
-            return vm.user = user;
-        });
-        $scope.$on('devise:logout', function(e, user){
-            return vm.user = {};
-        });
-
-    };
-
-    HomeController.$inject = ['$scope', 'Auth', '$state'];
+    HomeController.$inject = ['$scope']
 
     angular
         .module('flix4me')
-        .controller('HomeController', HomeController);
+        .controller('HomeController', HomeController)
+
+
+
 }());
