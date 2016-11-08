92fbdf47c43330464a6b5bf3a3416754f7b01de0
mcasciato <michael.casciato@icloud.com>
2016-11-08 11:18:09 -0500

    started tabs component

--- app/assets/javascripts/components/tab-component/tabs.component.js
+++ app/assets/javascripts/components/tab-component/tabs.component.js
@@ -1,5 +1,14 @@
 (function() {
 
+    'use strict';
+
+    var tabs = {
+        transclude: true,
+        controller: TabsController;
+        templateUrl: 'components/tab-component/tabs.html'
+    }
+
+    
     angular
         .module('flix4me')
         .component('tabs', tabs)
