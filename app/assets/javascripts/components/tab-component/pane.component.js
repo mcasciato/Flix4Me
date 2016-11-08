8d736acf1843ac205169697be4375775083af726
mcasciato <michael.casciato@icloud.com>
2016-11-08 11:30:51 -0500

    defined pane controller

--- app/assets/javascripts/components/tab-component/pane.component.js
+++ app/assets/javascripts/components/tab-component/pane.component.js
@@ -13,7 +13,13 @@
         controller: PaneController,
         templateUrl: 'components/tab-component/pane.html'
     }
-    
+
+    function PaneController() {
+        this.$onInit = function() {
+            this.TabsController.addPane(this)
+        }
+    }    
+
     angular
         .module('flix4me')
         .component('pane', pane)
