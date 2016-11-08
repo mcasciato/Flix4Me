312a94bc03620bdfb00e81f63452d7bc673936bf
mcasciato <michael.casciato@icloud.com>
2016-11-08 11:25:08 -0500

    more work on tabs controller

--- app/assets/javascripts/components/tab-component/tabs.component.js
+++ app/assets/javascripts/components/tab-component/tabs.component.js
@@ -8,7 +8,24 @@
         templateUrl: 'components/tab-component/tabs.html'
     }
 
-    
+    function TabsController() {
+        var panes = this.panes = []
+
+        this.select = function(pane) {
+            angular.forEach(panes, function(pane) {
+                pane.selected = false
+            });
+            pane.selected = true
+        }
+
+        this.addPane = function(pane) {
+            if (panes.length === 0) {
+                this.select(pane)
+            }
+            panes.push(pane)
+        }
+    }
+
     angular
         .module('flix4me')
         .component('tabs', tabs)
