f32e8b37d236c22ecd32004c30688f19a33222b1
mcasciato <michael.casciato@icloud.com>
2016-11-08 10:38:29 -0500

    simplified routes

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -3,8 +3,8 @@
 
     angular
         .module('flix4me', ['ui.router', 'templates', 'Devise'])
-        .config(['$httpProvider', function($httpProvider) {
+        .config(function($httpProvider) {
             // for CSRF errors
             $httpProvider.defaults.headers.common['X-CSRF-Token'] = $('meta[name=csrf-token]').attr('content');
-        }]);
+        });
 }());
--- app/assets/javascripts/routes.js
+++ app/assets/javascripts/routes.js
@@ -1,51 +1,25 @@
 (function() {
+
     'use strict';
 
     angular
         .module('flix4me')
-        .config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+        .config(function($stateProvider, $urlRouterProvider) {
             $stateProvider
                 .state('home', {
                     url: '/',
                     templateUrl: 'home/home.html',
-                    controller: 'HomeController as homeCtrl'
+                    controller: 'HomeController as vm'
                 })
-                .state('home.register', {
-                    url: 'register',
-                    templateUrl: 'auth/register.html',
-                    controller: 'AuthController as authCtrl',
-                    onEnter: ['$state', 'Auth', function($state, Auth) {
-                        Auth.currentUser().then(function(){
-                            $state.go('home.mylist');
-                        });
-                    }]
-                })
-                .state('home.login', {
-                    url: 'login',
-                    templateUrl: 'auth/login.html',
-                    controller: 'AuthController as authCtrl',
-                    onEnter: ['$state', 'Auth', function($state, Auth) {
-                        Auth.currentUser().then(function(){
-                            $state.go('home.mylist');
-                        });
-                    }]
-                })
-                .state('home.movies', {
-                    url: 'movies',
-                    templateUrl: 'movies/index.html',
-                    controller: 'MoviesController as moviesCtrl'
-                })
-                .state('home.tv_shows', {
-                    url: 'tv_shows',
-                    templateUrl: 'tv_shows/index.html',
-                    controller: 'TvShowsController as tvShowsCtrl'
-                })
-                .state('home.about', {
-                    url: 'about',
-                    templateUrl: 'about/about.html',
+                .state('home.profile', {
+                    url: 'profile',
+                    templateUrl: 'user/profile.html',
+                    controller: 'ProfileController as vm'
                 })
 
-                $urlRouterProvider.otherwise('/')
+            $urlRouterProvider.otherwise('/')
+        })
 
-        }]);
-}())
+
+
+}());
