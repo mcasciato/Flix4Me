81e0d947524f5ce05fec1c6f3f0e75c7aeaad425
mcasciato <michael.casciato@icloud.com>
2016-11-08 12:03:41 -0500

    finished movies var movies.component.js

--- app/assets/javascripts/components/movies/movies.component.js
+++ app/assets/javascripts/components/movies/movies.component.js
@@ -3,13 +3,15 @@
     'use strict';
 
     var movies = {
-
+        transclude: true,
+        controller: MovieComponentController,
+        templateUrl: 'components/movies/movies.html'
     }
 
     function MovieComponentController(MovieFactory) {
 
     }
-    
+
     angular
         .module('flix4me')
         .component('movies', movies)
