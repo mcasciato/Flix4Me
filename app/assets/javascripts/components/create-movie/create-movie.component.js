82c61c06243955bc0956c2c8e2c1444bc46edc2a
mcasciato <michael.casciato@icloud.com>
2016-11-09 11:04:55 -0500

    more work on create movie component

--- app/assets/javascripts/components/create-movie/create-movie.component.js
+++ app/assets/javascripts/components/create-movie/create-movie.component.js
@@ -3,9 +3,14 @@
     'use strict';
 
     var createMovie = {
+        transclude: true,
+        controller: CreateMovieComponentController,
+        templateUrl: 'components/create-movie/create-movie.html'
+    }
+    function CreateMovieComponentController() {
 
     }
-    
+
     angular
         .module('flix4me')
         .component('createMovie', createMovie)
