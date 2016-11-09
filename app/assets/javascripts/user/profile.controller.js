781a608a0a93853586e360e5f2f7bc1482b2d156
mcasciato <michael.casciato@icloud.com>
2016-11-09 16:35:39 -0500

    success: cannot view profile unless logged in

--- app/assets/javascripts/user/profile.controller.js
+++ app/assets/javascripts/user/profile.controller.js
@@ -2,8 +2,17 @@
 
     'use strict';
 
-    function ProfileController() {
+    function ProfileController(Auth, $state) {
+        var vm = this
 
+        vm.signedIn = Auth.isAuthenticated();
+
+        if (vm.signedIn) {
+            $state.go('home.profile')
+        } else {
+            alert("You must be logged in to view your profile")
+            $state.go('home.login')
+        }
     };
 
     angular
