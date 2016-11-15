4f2b409e1faceffaa42a151601d3dc32be4ddf70
mcasciato <michael.casciato@icloud.com>
2016-11-15 16:53:36 -0500

    added ngMessages

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -2,7 +2,7 @@
     'use strict';
 
     angular
-        .module('flix4me', ['ui.router', 'templates', 'Devise'])
+        .module('flix4me', ['ui.router', 'templates', 'Devise', 'ngMessages'])
         .config(function($httpProvider) {
             // for CSRF errors
             $httpProvider.defaults.headers.common['X-CSRF-Token'] = $('meta[name=csrf-token]').attr('content');
