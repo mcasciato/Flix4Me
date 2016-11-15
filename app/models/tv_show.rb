c08e002e6f5defbc4b003f97edaa0b6801f90c9f
mcasciato <michael.casciato@icloud.com>
2016-11-14 19:33:10 -0500

    deleted tv show controller, model, and serializer

--- app/models/tv_show.rb
+++
@@ -1,3 +0,0 @@
-class TvShow < ApplicationRecord
-    belongs_to :user
-end
--- app/serializers/tv_show_serializer.rb
+++
@@ -1,7 +0,0 @@
-class TvShowSerializer < ActiveModel::Serializer
-
-    attributes :id, :user_id, :title, :year, :genre, :plot, :rating
-
-    has_one :user
-
-end
