903faea75cf3ab4c3e420a31b26afb7324493300
mcasciato <michael.casciato@icloud.com>
2016-11-15 10:49:56 -0500

    began defining getMovies function

--- app/assets/javascripts/movies/movies.controller.js
+++ app/assets/javascripts/movies/movies.controller.js
@@ -2,7 +2,21 @@
     'use strict';
 
     function MoviesController(MovieFactory, $filter, $state, Auth) {
+        var vm = this;
 
+        // callable methods
+        vm.getMovies = getMovies;
+
+        // instantiate
+        activate();
+
+        function activate() {
+            getMovies();
+        }
+
+        function getMovies() {
+            
+        }
     }
 
     MoviesController.$inject = ['MovieFactory', '$filter', '$state', 'Auth'];
