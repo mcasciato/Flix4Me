298c0cc19b452bbad4a48e4468ad34a11fdbda9a
mcasciato <michael.casciato@icloud.com>
2016-11-28 14:15:11 -0500

    finished comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,18 @@
 class CommentsController < ApplicationController
     def index
-
+        comments = Comment.all
+        render json: comments
     end
 
     def create
-
+        movie = Movie.find_by_id(params[:movie_id])
+        comment = movie.comments.new(comment_params)
+            if comment.save
+                render json: comment
+            else
+                render json:
+                    { errors: comment.errors.full_messages },
+                    status: 422
     end
 
     private
