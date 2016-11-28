f586140c337d4adbadcd25e1cab0c4e744d76dac
mcasciato <michael.casciato@icloud.com>
2016-11-28 15:43:25 -0500

    more work on comment factory; defined function

--- app/assets/javascripts/comments/comment.factory.js
+++ app/assets/javascripts/comments/comment.factory.js
@@ -1,6 +1,14 @@
 (function(){
     'use strict';
 
+        function CommentFactory($http) {
+            return {
+                newComment: newComment,
+            }
+
+        }
+
+        CommentFactory.$inject = ['$http'];
 
     angular
         .module('flix4me')
