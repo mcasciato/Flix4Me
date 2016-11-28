39e9c79dcee098c82ff06b885f0e70b244424f6d
mcasciato <michael.casciato@icloud.com>
2016-11-28 15:46:38 -0500

    comment component 50% done

--- app/assets/javascripts/comments/comment.component.js
+++ app/assets/javascripts/comments/comment.component.js
@@ -1,6 +1,18 @@
 (function(){
     'use strict';
 
+    var comments = {
+      transclude: true,
+      templateUrl: 'comments/comments.html',
+      controller: NewCommentController
+    };
+
+    function NewCommentController($stateParams, $state, CommentFactory) {
+
+    };
+
+    NewCommentController.$inject = ['$stateParams', '$state', 'CommentFactory'];
+
     angular
         .module('flix4me')
         .component('comments', comments)
