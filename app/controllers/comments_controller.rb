6ec892d384180c95b5945bf8fb574858453ac4d0
mcasciato <michael.casciato@icloud.com>
2016-11-29 15:18:19 -0500

    updated user/comment association; refactored comments controller to accept user/movie ids

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,8 +5,10 @@ class CommentsController < ApplicationController
     end
 
     def create
-        movie = Movie.find_by_id(params[:movie_id])
-        comment = movie.comments.new(comment_params)
+        movie = current_user.movies.find_by_id(params[:movie_id])
+        comment = Comment.new(comment_params)
+        comment.user_id = current_user.id
+        comment.movie_id = params[:movie_id]
             if comment.save
                 render json: comment
             else
@@ -19,6 +21,6 @@ class CommentsController < ApplicationController
     private
 
         def comment_params
-            params.require(:comment).permit(:content, :movie_id)
+            params.require(:comment).permit(:content, :movie_id, :user_id)
         end
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -3,5 +3,6 @@ class UserSerializer < ActiveModel::Serializer
     attributes :id, :username, :email
 
     has_many :movies
+    has_many :comments
 
 end
