8e836271f1f7d7949800d4bfe2e7548516ee6e50
mcasciato <michael.casciato@icloud.com>
2016-12-01 10:53:04 -0500

    removed views from movie controller params and serializer

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -57,6 +57,6 @@ class MoviesController < ApplicationController
     private
 
     def movie_params
-        params.require(:movie).permit(:title, :year, :genre, :plot, :rating, :views)
+        params.require(:movie).permit(:title, :year, :genre, :plot, :rating)
     end
 end
--- app/serializers/movie_serializer.rb
+++ app/serializers/movie_serializer.rb
@@ -1,6 +1,6 @@
 class MovieSerializer < ActiveModel::Serializer
 
-    attributes :id, :user_id, :title, :year, :genre, :plot, :rating, :views
+    attributes :id, :user_id, :title, :year, :genre, :plot, :rating
 
     has_one :user
     has_many :comments
