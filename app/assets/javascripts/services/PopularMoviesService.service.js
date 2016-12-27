3b4e4d16f72a725afddff2313d4ff3be447d0c03
mcasciato <michael.casciato@icloud.com>
2016-12-27 12:07:16 -0500

    added url for API -- currently has api_key in it, will be removed soon

--- app/assets/javascripts/services/PopularMoviesService.service.js
+++ app/assets/javascripts/services/PopularMoviesService.service.js
@@ -3,7 +3,9 @@
     'use strict';
 
     function PopularMoviesService($http) {
-
+        this.getPopularMovies = function () {
+            return $http.get('https://api.themoviedb.org/3/movie/popular?api_key=47fdd44646c7f80f91ffd886d8b52d00&language=en-US&page=1')
+        };
     }
 
     angular
