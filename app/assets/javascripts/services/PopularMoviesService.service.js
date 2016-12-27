b47e2ce20589733f0fe6259527ae7fd73898864a
mcasciato <michael.casciato@icloud.com>
2016-12-27 11:39:52 -0500

    changed name from TopMovies... to PopularMovies...

---
+++ app/assets/javascripts/services/PopularMoviesService.service.js
@@ -0,0 +1,12 @@
+(function() {
+
+    'use strict';
+
+    function PopularMoviesService($http) {
+
+    }
+
+    angular
+        .module('flix4me')
+        .service('PopularMoviesService', PopularMoviesService);
+}()):
