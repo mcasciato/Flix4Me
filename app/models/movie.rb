8feea3fd2476a589a13b8d1344b0c0fd2e72d762
mcasciato <michael.casciato@icloud.com>
2017-01-24 22:52:42 -0500

    wired up Movie model to paperclip methods

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,9 @@
 class Movie < ApplicationRecord
     belongs_to :user
     has_many :comments
+    has_attached_file :image
+    validates_attachment_content_type :iamge, content_type: /\Aimage\/.*\z/
+
 
     def as_json(options = {})
 		super(options.merge(include: [:user, comments: {include: :user}] ))
