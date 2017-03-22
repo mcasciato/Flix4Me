afc3d6f1ad599a6d8024de05b6161f05132e6979
mcasciato <michael.casciato@icloud.com>
2017-03-22 18:43:45 -0400

    attempt to fix csrf error for angular devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,9 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   respond_to :json
   before_action :configure_permitted_parameters, if: :devise_controller?
-  after_filter :set_csrf_cookie
+  after_filter :set_csrf_cookie_for_ng
 
-  def set_csrf_cookie
+  def set_csrf_cookie_for_ng
       cookies['XSRF-TOKEN'] = form_authenticity_token if protect_against_forgery?
   end
 
@@ -15,7 +15,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def verified_request?
-      super || valid_authenticity_token?(session, request.headers['X-XSRF-TOKEN'])
+    super || valid_authenticity_token?(session, request.headers['X-XSRF-TOKEN'])
   end
 
   def configure_permitted_parameters
